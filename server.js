a63da32ed07558d1f727db9ed180e3f846125c3d
Dawchih Liou <dawochih.liou@gmail.com>
2016-08-01 03:40:29 +0000

    add token protected routes

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var User       = require('./app/models/user');
 
 var port       = process.env.PORT || 8080;
 
+var routes     = express.Router();
+
 mongoose.connect(config.database);
 
 app.set('superSecret', config.secret);
@@ -40,6 +42,70 @@ app.get('/setup', function (req, res) {
         res.json({ success: true });
     });
 });
+
+routes.post('/authenticate', function (req, res) {
+    var name = req.body.name;
+    var password = req.body.password;
+    
+    User.findOne({
+       name: name,
+       password: password
+    }, function (err, user) {
+        var token;
+        
+        if (err) throw err;
+        
+        if (!user) {
+            res.json({ sucess: false, message: 'Authenticateion failed. ' + name + ' not found' });
+        } else {
+            token = jwt.sign(user, app.get('superSecret'), {
+                expiresIn: 60*60*24
+            });
+            
+            res.json({
+                success: true,
+                message: 'Enjoy your token!',
+                token: token
+            });
+        }
+    });
+});
+
+
+routes.use(function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({success: false, message: 'Failed to authenticate token.' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(403).send({
+           success: false,
+           message: 'No token provided'
+        });
+    }
+});
+
+routes.get('/', function (req, res) {
+   res.json({ message: 'Welcome to the coolest API on earth!' }); 
+});
+
+routes.get('/users', function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) throw err;
+        
+        res.json(users)
+    });
+});
+
+
+app.use('/api', routes);
 
 
 app.listen(port);
